f5f903f763034e46a45ca833a2124a6416176852
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2019-12-22 19:59:13 +0100

    created my first sample chart.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Line } from "react-chartjs-2";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: {
+        labels: ["1", "2", "3", "4", "5", "6", "7"],
+        datasets: [
+          {
+            label: "Video",
+            backgroundColor: "rgba(24, 24, 24, 0.8)",
+            data: [1, 3, 4, 56, 57, 67, 80]
+          },
+          {
+            label: "Audio",
+            backgroundColor: "rgba(255, 0, 255, 0.8)",
+            data: [7, 23, 9, 2, 19, 45, 25]
+          }
+        ]
+      }
+    }
+  }
+
+
+  setGradientColor = (canvas, color) => {
+    const ctx = canvas.getContext('2d');
+    console.log(ctx);
+    const gradient = ctx.createLinearGradient(0, 0, 600, 550);
+    gradient.addColorStop(0, color);
+    gradient.addColorStop(0.95, "rgba(255, 0, 255, 0.8)");
+    return gradient;
+  }
+
+  getChartData = canvas => {
+    const data = this.state.data;
+    if (data.datasets) {
+      let colors = ["rgba(24, 24, 24, 0.8)", "rgba(255, 0, 255, 0.8)"];
+      data.datasets.forEach((set, i) => {
+        set.backgroundColor = this.setGradientColor(canvas, colors[i]);
+        set.borderColor = colors[i];
+        set.borderWidth = 2;
+      });
+    }
+
+    return data;
+  }
+
+  render() {
+    return (
+      <div style={{position: "relative", width: 800, height: 750}}>
+        <h3> Sample of a chart </h3>
+          <Line
+            options={{
+              responsive: true
+            }}
+            data={this.getChartData}
+          />
+      </div>
+    )
+  }
 }
 
-export default App;
+
+
+
+
